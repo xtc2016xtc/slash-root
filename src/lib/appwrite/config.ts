5e47dbc8223be35f9c842a333f8737df8edb0544
xtc2016xtc <728611827@qq.com>
2025-01-26 16:02:30 +0800

    gengx

---
+++ src/lib/appwrite/config.ts
@@ -0,0 +1,23 @@
+import { Client, Account, Databases, Storage, Avatars } from 'appwrite';
+
+
+export const appwriteConfig = {
+  url:import.meta.env.VITE_APPWRITE_URL,
+  projectId:import.meta.env.VITE_APPWRITE_PROJECT_ID,
+  databaseId: import.meta.env.VITE_APPWRITE_DATASE_ID,
+  usersid:import.meta.env.VITE_APPWRITE_USER_ID,
+  contextid:import.meta.env.VITE_APPWRITE_CONTEXT_ID
+}
+
+export const client = new Client();
+
+
+client.setProject(appwriteConfig.projectId);
+client.setEndpoint(appwriteConfig.url);
+
+
+export const account = new Account(client);
+export const databases = new Databases(client);
+export const storage = new Storage(client);
+export const avatars = new Avatars(client);
+
